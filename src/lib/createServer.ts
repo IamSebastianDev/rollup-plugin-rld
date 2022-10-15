4b32e8c2ade3ea91d0ed37c365dc11e1da1701bc
Sebastian Heinz <sebastian.heinz@adabay.rocks>
2022-10-15 12:43:41 +0200

    fix: resolve issue that would lead to server not recognizing correct url

--- src/lib/createServer.ts
+++ src/lib/createServer.ts
@@ -15,7 +15,8 @@ import { logSuccess } from '../utils/logSuccess.util';
 
 export const createServer = (serverInit: ServerInit): Server => {
     return http.createServer(async (req, res) => {
-        const { emitter, url } = serverInit;
+        let { emitter, url } = serverInit;
+        url = url[0] === '/' ? url : '/' + url;
 
         // if the passed url is different from the request url,
         // cancel the request.
