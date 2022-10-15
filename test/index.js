2618df24ba175667420ccdf51e6c9c2833109af3
Sebastian Heinz <sebastian.heinz@adabay.rocks>
2022-10-15 11:20:02 +0200

    feat: add initial tests

---
+++ test/index.js
@@ -0,0 +1,16 @@
+/** @format */
+import test from 'ava';
+import { rld } from '../dist/index.mjs';
+
+test(`Plugin is a Factory Function`, (t) => {
+    t.is(typeof rld, 'function');
+});
+
+test(`Plugin instances correctly`, (t) => {
+    const plugin = rld();
+    t.is(typeof plugin, 'object');
+    t.is(typeof plugin.buildStart, 'function');
+    t.is(typeof plugin.transform, 'function');
+    t.is(typeof plugin.closeWatcher, 'function');
+    t.is(plugin.name, 'Rollup-Plugin-Rld');
+});
