a50d2e48ba7ef8b87c2b7747b262583bcf8f4e13
Sebastian <sebastian@iamsebastian.dev>
2023-05-20 16:27:23 +0200

    feat: add option to log a reload message in the browser A optional config property to indicate that the browser was reloaded.

--- src/lib/injectable.ts
+++ src/lib/injectable.ts
@@ -1,6 +1,6 @@
 /** @format */
 
-export const injectable = (port: number | string, host: string, url: string, attributes: string) => {
+export const injectable = (port: number | string, host: string, url: string, attributes: string, log: boolean) => {
     // Return early if there is no window or document or the script tag already exists.
     if (!window || !document) return;
     if (document.head.querySelector(`[url='${url}']`)) return;
@@ -10,7 +10,9 @@ export const injectable = (port: number | string, host: string, url: string, att
         script.setAttribute(key, value)
     );
     script.setAttribute('url', url);
-    script.textContent = `new EventSource('http://${host}:${port}/${url}').addEventListener('message', (ev) => JSON.parse(ev.data).rld && window.navigation.reload());`;
+    script.textContent = `new EventSource('http://${host}:${port}/${url}').addEventListener('message', (ev) => 
+        JSON.parse(ev.data).rld && window.navigation.reload())
+        ${log ? `console.log('[Rollup-Plugin-Rld]: Reloaded...');` : ''}`;
 
     // Inject a comment before the script tag to mark the script as useful
     document.head.appendChild(document.createComment(' Script injected by Rollup-Plugin-Rld '));
